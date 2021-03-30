c5526afcc7fdd4411c6584e7946675ecc8fe662a
Bmediax <contact@bmediax.com>
2021-03-30 10:25:24 -0700

    :sparkles: Animation for Coming Soon Page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import * as React from "react"
 import { StaticImage } from "gatsby-plugin-image"
+import { motion } from 'framer-motion'
 import Layout from '../layout'
 import * as comingStyles from '../styles/comingSoon.module.scss'
 import { AiFillInstagram, AiFillYoutube } from 'react-icons/ai'
@@ -10,9 +11,19 @@ const IndexPage = () => {
   return (
     <Layout title="Coming Soon | Mae Study">
       <div className={comingStyles.comingsoon}>
-        <h1> Coming Soon </h1>
-        <StaticImage src="../images/gfx/maestudy-logo.png" alt="Mae Study Logo" imgClassName="logo" width={400} placeholder="BLURRED" />
-        <div className={comingStyles.smicons}>
+        <motion.h1 initial={{ y:50 }} animate={{ y:0 }} transition={{ delay: 0.05, type: 'spring' }}> Coming Soon </motion.h1>
+        <motion.div 
+          initial={{ y:100 }} 
+          animate={{ y:0 }} 
+          transition={{ type:'spring', damping: 10 }}
+          whileHover={{ scale: 1.05 }}>
+          <StaticImage src="../images/gfx/maestudy-logo.png" alt="Mae Study Logo" imgClassName="logo" width={400} placeholder="BLURRED" />
+        </motion.div>
+        <motion.div initial={{ y:100 }} 
+          animate={{ y:0 }} 
+          transition={{ type:'spring', damping: 10, stiffness:80 }}
+          whileHover={{ scale: 1.05 }} 
+          className={comingStyles.smicons}>
             <a href="mailto:hello@maestudy">
                 <MdEmail />
             </a>
@@ -22,7 +33,7 @@ const IndexPage = () => {
             <a href="https://twitter.com/maestudy">
                 <AiFillYoutube />
             </a>
-        </div>
+        </motion.div>
       </div>
     </Layout>
   )
