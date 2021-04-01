3e71627ad9da83c3c104d55edeecc6738c35dacb
Bmediax <contact@bmediax.com>
2021-03-31 17:36:29 -0700

    Logo Added

--- src/components/Links/LinkHeader/LinkHeader.js
+++ src/components/Links/LinkHeader/LinkHeader.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import * as linkheaderStyles from './LinkHeader.module.scss'
 import LinkSocialMedia from '../LinkSocialMedia/LinkSocialMedia'
+import { StaticImage } from "gatsby-plugin-image"
 
 const LinkHeader = () => {
     return (
         <div className={linkheaderStyles.linkHeader}>
-            <div className={linkheaderStyles.profilePic}></div>
+            <div className={linkheaderStyles.profilePic}>
+                <StaticImage src='../../../images/gfx/maestudy-logo.png' alt="Mae Study Profile Picture" loading="eager" placeholder="blurred" />
+            </div>
             <LinkSocialMedia />
         </div>
     )
