41c5c8d1b0e4ca6b22a12e4f691ae872402f6e8c
Bmediax <contact@bmediax.com>
2021-04-01 10:55:14 -0700

    Changed updated

--- src/components/Links/LinkLatestVideo/index.js
+++ src/components/Links/LinkLatestVideo/index.js
@@ -6,7 +6,7 @@ import LinksSection from '../../../layout/LinksSection'
 const LinkLatestVideo = () => {
     return (
         <LinksSection>
-            <motion.div initial={{ bottom:100 }} animate={{ bottom:0 } transition={{ duration: .7 }} className={LinkVideoStyles.latestVideo}> 
+            <motion.div initial={{ bottom:100 }} animate={{ bottom:0 }} transition={{ duration: .7 }} className={LinkVideoStyles.latestVideo}> 
                 <img src="https://via.placeholder.com/1335x750?text=Latest%20Video" alt="Latest Mae Study Video" />
             </motion.div>
         </LinksSection>
