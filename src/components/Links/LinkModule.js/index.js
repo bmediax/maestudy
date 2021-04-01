a7f6909f0a0f29f763d93ba83458fb2821ad4e3b
Bmediax <contact@bmediax.com>
2021-04-01 09:59:15 -0700

    :sparkles: Links Data ADDED

--- src/components/Links/LinkModule.js/index.js
+++ src/components/Links/LinkModule.js/index.js
@@ -2,19 +2,15 @@ import React from 'react'
 import LinksSection from '../../../layout/LinksSection'
 import LinkObject from '../LinkObject'
 import * as linkModuleStyle from './linkModule.module.scss'
+import { linksData } from '../../../data/linksData'
 
 const LinkModule = () => {
     return (
         <LinksSection>
             <div className={linkModuleStyle.linkModule}>
-                <LinkObject title="Amazon Gift Card Giveaway"/>
-                <LinkObject title="Amazon Gift Card Giveaway"/>
-                <LinkObject title="Amazon Gift Card Giveaway"/>
-                <LinkObject title="Amazon Gift Card Giveaway"/>
-                <LinkObject title="Amazon Gift Card Giveaway"/>
-                <LinkObject title="Amazon Gift Card Giveaway"/>
-                <LinkObject title="Amazon Gift Card Giveaway"/>
-                <LinkObject title="Amazon Gift Card Giveaway"/>
+                {linksData.map(links => (
+                    <LinkObject title={links.title} key={links.id} />
+                ))}
             </div>
         </LinksSection>
     )
---
+++ src/data/linksData.js
@@ -0,0 +1,30 @@
+export const linksData = [
+    {
+        title: 'Amazon Gift Card Give Away',
+        id: 1
+    },
+    {
+        title: 'Amazon Gift Card Give Away',
+        id: 2
+    },
+    {
+        title: 'Amazon Gift Card Give Away',
+        id: 3
+    },
+    {
+        title: 'Amazon Gift Card Give Away',
+        id: 4
+    },
+    {
+        title: 'Amazon Gift Card Give Away',
+        id: 5
+    },
+    {
+        title: 'Amazon Gift Card Give Away',
+        id: 6
+    },
+    {
+        title: 'Amazon Gift Card Give Away',
+        id: 7
+    },
+]
