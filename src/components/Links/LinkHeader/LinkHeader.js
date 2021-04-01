2c486aed465e5a62b6de7278047874770276acb9
bmediax <contact@bmediax.com>
2021-03-31 23:07:27 -0700

    Gifs Added

--- src/components/Links/LinkHeader/LinkHeader.js
+++ src/components/Links/LinkHeader/LinkHeader.js
@@ -1,15 +1,20 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 import * as linkheaderStyles from './LinkHeader.module.scss'
 import LinkSocialMedia from '../LinkSocialMedia/LinkSocialMedia'
 import { StaticImage } from "gatsby-plugin-image"
+import maeStudyBanner from '../../../images/gfx/maestudy-banner.gif'
 
 const LinkHeader = () => {
     return (
         <div className={linkheaderStyles.linkHeader}>
-            <div className={linkheaderStyles.profilePic}>
+            <motion.div initial={{ y: 100, opacity:0 }} animate={{ y:0, opacity:1 }} transition={{ type: 'spring', damping:8, stiffness:100 }} className={linkheaderStyles.profilePic}>
                 <StaticImage src='../../../images/gfx/maestudy-logo.png' alt="Mae Study Profile Picture" loading="eager" placeholder="blurred" />
+            </motion.div>
+            <div className={linkheaderStyles.profileSide}>
+                <img src={maeStudyBanner} alt="Mae Study Banner" />
+                <LinkSocialMedia />
             </div>
-            <LinkSocialMedia />
         </div>
     )
 }
