4f39b82bed2c26e2aa6c009b0c3c47120a111aa2
bmediax <contact@bmediax.com>
2021-04-01 22:01:13 -0700

    :sparkles: Animated bar

--- src/components/Links/LinkBtmBannerModule/index.js
+++ src/components/Links/LinkBtmBannerModule/index.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 import * as linkBtmBannerStyles from './LinkBtmBannerModule.module.scss'
 import { BsArrowUpShort } from 'react-icons/bs'
 
 const LinkBtmBannerModule = () => {
     return (
-        <div className={linkBtmBannerStyles.bannerModule}> 
+        <motion.a initial={{ opacity: 0 }} animate={{ opacity:1 }} transition={{ delay:.4, duration:.8 }} href="https://www.stopasianhate.info/" className={linkBtmBannerStyles.bannerModule}>
             <p>#StopAsianHate</p>
             <BsArrowUpShort />
-        </div>
+        </motion.a>
     )
 }
 
