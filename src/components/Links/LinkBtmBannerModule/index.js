7e62ad3d02ce5b68d1dc4f6af23a1e57713c4eee
Bmediax <contact@bmediax.com>
2021-04-02 10:45:25 -0700

    :sparkles: Changed Font to Playfair

--- src/components/Links/LinkBtmBannerModule/index.js
+++ src/components/Links/LinkBtmBannerModule/index.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { motion } from 'framer-motion'
 import * as linkBtmBannerStyles from './LinkBtmBannerModule.module.scss'
-import { BsArrowUpShort } from 'react-icons/bs'
+import { AiOutlineLink } from 'react-icons/ai'
 
 const LinkBtmBannerModule = () => {
     return (
         <motion.a initial={{ opacity: 0 }} animate={{ opacity:1 }} transition={{ delay:.4, duration:.8 }} href="https://www.stopasianhate.info/" className={linkBtmBannerStyles.bannerModule}>
             <p>#StopAsianHate</p>
-            <BsArrowUpShort />
+            <AiOutlineLink />
         </motion.a>
     )
 }
