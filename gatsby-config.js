d9e64cf11edf462fc29fda4b60c180ad34c31562
Bmediax <contact@bmediax.com>
2021-04-02 10:59:07 -0700

    :sparkles: Favicon Added

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,18 @@ module.exports = {
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
     {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Mae Study`,
+        short_name: `MaeStudy`,
+        start_url: `/`,
+        background_color: `#f6a998`,
+        theme_color: `#f6a998`,
+        display: `standalone`,
+        icon: `src/images/gfx/icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
       resolve: "gatsby-source-filesystem",
       options: {
         name: "images",
